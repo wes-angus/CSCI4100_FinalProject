f7a1718bdb0db586345cd298252fc623fe4fb65f
wes-angus <wesley.angus@uoit.net>
2015-11-26 23:42:52 -0500

    Added sound
    Added alternate sound for sound effects + disclaimer.

--- CSCI4100_Final-Project/app/src/main/java/csci4100/uoit/ca/csci4100_final_project/AboutActivity.java
+++ CSCI4100_Final-Project/app/src/main/java/csci4100/uoit/ca/csci4100_final_project/AboutActivity.java
@@ -1,17 +1,33 @@
 package csci4100.uoit.ca.csci4100_final_project;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.SpannableString;
+import android.text.style.StyleSpan;
 import android.view.View;
+import android.widget.TextView;
 
-import csci4100.uoit.ca.csci4100_final_project.R;
-
-public class AboutActivity extends Activity {
-
+public class AboutActivity extends Activity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        String str = getString(R.string.about_txt);
+        int i_index = str.indexOf("\n");
+        int b_index1_start = str.indexOf(":") + 1;
+        int b_index1_end = str.indexOf("\n", b_index1_start);
+        int b_index2 = str.indexOf(":", b_index1_end) + 1;
+        str = str.replace("*", "");
+        SpannableString text = new SpannableString(str);
+        text.setSpan(new StyleSpan(Typeface.ITALIC), 0, i_index, 0);
+        text.setSpan(new StyleSpan(Typeface.BOLD), b_index1_start, b_index1_end, 0);
+        text.setSpan(new StyleSpan(Typeface.BOLD), b_index2, text.length(), 0);
+        TextView textView = (TextView) findViewById(R.id.about_tView);
+        textView.setText(text, TextView.BufferType.SPANNABLE);
     }
 
     public void backToMenu(View view)
